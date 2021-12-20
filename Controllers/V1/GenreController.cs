6c7bcd39f9c5789b2f55a2213bfdfab608aab258
Kerroline <Kerroline86@gmail.com>
2021-12-20 15:05:53 +0500

    CRUD Genre model

--- Controllers/V1/GenreController.cs
+++ Controllers/V1/GenreController.cs
@@ -24,12 +24,51 @@ namespace MangaCMS.Controllers.V1
             _mangaContext = context;
         }
 
+        /// <summary>
+        /// Get all genres
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET /GetAll
+        ///     {
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">List of Genres</response>
+        [Route("GetAll")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<GenreModel>>> GetAll()
         {
             return await _mangaContext.Genres.ToListAsync();
         }
 
+        /// <summary>
+        /// Get once genre.
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     GET /GetOnce/1
+        ///     {
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">Genre by id</response>
+        /// <response code="400">Genre is not found</response>
+        [Route("GetOnce")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GenreModel>> GetOnce(int id)
+        {
+            var currentGenre = await _mangaContext.Genres.FindAsync(id);
+            if (currentGenre is not null)
+            {
+                return currentGenre;
+            }
+            return StatusCode(400, "Genre is not found");
+        }
 
         /// <summary>
         /// Creates a new Genre.
@@ -69,6 +108,69 @@ namespace MangaCMS.Controllers.V1
                 return StatusCode(204, "Genre created");
             }
             return StatusCode(400, "Model invalid");
+        }
+
+        /// <summary>
+        /// Edit an exist genre
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="genre"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Edit/1
+        ///     {
+        ///        "GenreName": "newName"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="204">If the genre is update</response>
+        /// <response code="400">If the genre is not found</response>
+        [Route("Edit")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GenreModel>> Edit(int id, GenreModel genre)
+        {
+            var currentGenre = await _mangaContext.Genres.FindAsync(id);
+            if (currentGenre is not null)
+            {
+                if (ModelState.IsValid)
+                {
+                    currentGenre.GenreName = genre.GenreName;
+                    await _mangaContext.SaveChangesAsync();
+                    return StatusCode(200, "A GenreName is update");
+                }
+            }
+            return StatusCode(400, "A Genre is not found");
+        }
+
+        /// <summary>
+        /// Delete an exist genre
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     DELETE /Delete/1
+        ///     {
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="204">If the status is deleted</response>
+        /// <response code="400">If the status is not found</response>
+        [Route("Delete")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<GenreModel>> Delete(int id)
+        {
+            var currentGenre = await _mangaContext.Genres.FindAsync(id);
+            if (currentGenre is not null)
+            {
+                _mangaContext.Genres.Remove(currentGenre);
+                await _mangaContext.SaveChangesAsync();
+                return StatusCode(200, "A Genre deleted successfully");
+            }
+            return StatusCode(400, "A Genre is not found");
         }
 
 
