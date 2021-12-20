cc109645c2f973c97b9b4da3fbf9aac427519880
Kerroline <Kerroline86@gmail.com>
2021-12-20 12:24:27 +0500

    Add swagger jwt key

--- Startup.cs
+++ Startup.cs
@@ -108,6 +108,27 @@ namespace MangaCMS
                     Version = "v1",
                     Title = "MangaCMS API"
                 });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+                {
+                    {
+                         new OpenApiSecurityScheme
+                         {
+                               Reference = new OpenApiReference
+                               {
+                                     Type = ReferenceType.SecurityScheme,
+                                     Id = "Bearer"
+                               }
+                         },
+                         new string[] { }
+                    }
+                });
 
                 //Set the comments path for the swagger json and ui.
                 var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
